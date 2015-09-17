73ef1d839b8960f6bfb655c5186fadf870398f30
unknown <s1090832@student.hsleiden.nl>
2015-09-17 10:59:36 +0200

    Add notitie, mail and opmerking table

--- src/DB/database.sql
+++ src/DB/database.sql
@@ -1,3 +1,24 @@
 CREATE TABLE factuur (
 
 )
+
+CREATE TABLE notitie (
+id INTEGER(11) NOT NULL PRIMARY KEY,
+titel VARCHAR(255) NOT NULL,
+notitie TEXT NOT NULL
+)
+
+CREATE TABLE mail (
+id INTEGER(11) NOT NULL PRIMARY KEY,
+debiteur_id INTEGER(11) NOT NULL FOREIGN KEY (debiteur_id) REFERENCES debiteur(id),
+factuur_id INTEGER(11) NOT NULL FOREIGN KEY (factuur_id) REFERENCES factuur(id),
+organisatie_id INTEGER(11) NOT NULL FOREIGN KEY (organisatie_id) REFERENCES organisatie(id),
+header VARCHAR(255) NOT NULL,
+body VARCHAR(255) NOT NULL,
+footer VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE opmerking (
+id INTEGER(255) NOT NULL PRIMARY KEY,
+opmerking TEXT NOT NULL
+)
