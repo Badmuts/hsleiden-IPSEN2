f828ad2892283caf1fd4570cb24c000b55ae124c
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-17 09:28:03 +0200

    Setup project

---
+++ src/Panthera.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Panthera extends Application {
+
+    private Stage stage;
+
+    public Panthera getInstance() {
+        return this;
+    }
+
+    @Override public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        this.stage.setScene(new Scene(new TextField("Panthera")));
+        System.out.println("Panthera");
+    }
+}
