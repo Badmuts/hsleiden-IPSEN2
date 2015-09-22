c3fde2a71064811097e4330f2a979c44ce9ade49
Brandon van Wijk <brandonvanwijk@gmail.com>
2015-09-22 16:47:46 +0200

    Factuur klasse gemaakt met attributen en methodes

---
+++ src/Models/Factuur.java
@@ -0,0 +1,77 @@
+package Models;
+
+import java.util.Calendar;
+
+/**
+ * Created by Brandon on 22-Sep-15.
+ */
+public class Factuur {
+    private int id;
+    private Calendar factuurdatum;
+    private Calendar vervaldatum;
+    private Debiteur debiteur;
+    private OrderRegel orderRegel;
+    private String opmerking;
+    private String notitie;
+    private Organisatie organisatie;
+
+    public Factuur() {
+        FactuurDAO factuurDAO = new FactuurDAO();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Factuur getFactuurById() {
+        factuurDAO.getFactuur(getId());
+    }
+    public Factuur getAllFacturen() {
+        factuurDAO.getAllFacturen();
+    }
+
+    public Calendar getFactuurdatum() {
+        return factuurdatum;
+    }
+
+    public Calendar getVervaldatum() {
+        return vervaldatum;
+    }
+
+    public Debiteur getDebiteur() {
+        return debiteur;
+    }
+
+    public OrderRegel getOrderRegel() {
+        return orderRegel;
+    }
+
+    public Organisatie getOrganisatie() {
+        return organisatie;
+    }
+
+    public String getOpmerking() {
+        return opmerking;
+    }
+
+    public String getNotitie() {
+        return notitie;
+    }
+
+    public void setOpmerking(String opmerking) {
+        this.opmerking = opmerking;
+    }
+
+    public void setNotitie(String notitie) {
+        this.notitie = notitie;
+    }
+
+    public void deleteFactuurFromDatabaseById() {
+        factuurDAO.deleteFactuur(getId());
+    }
+
+    public void addFactuurToDatabase() {
+        factuurDAO.saveFactuur(factuurdatum, vervaldatum, debiteur, orderRegel, opmerking, notitie, organisatie);
+    }
+}
+
+
