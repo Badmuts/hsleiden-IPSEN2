739d193317dee3857063b488559fee1785533185
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-22 21:26:06 +0200

    Add singleton pattern to main class and setup its first view

--- src/Panthera.java
+++ src/Panthera.java
@@ -5,15 +5,49 @@ import javafx.stage.Stage;
 
 public class Panthera extends Application {
 
+    private static Panthera uniqueInstance;
     private Stage stage;
 
-    public Panthera getInstance() {
-        return this;
+    /**
+     * JavaFX Singleton constructor.
+     */
+    public Panthera() {
+        super();
+        synchronized(Panthera.class){
+            if(uniqueInstance != null) throw new UnsupportedOperationException(
+                    getClass()+" is singleton but constructor called more than once");
+            uniqueInstance = this;
+        }
     }
 
+    /**
+     * Returns the instance of this object (because it is a Singleton)
+     *
+     * @return Panthera Singleton Panthera object.
+     */
+    public Panthera getInstance() {
+        return uniqueInstance;
+    }
+
+    /**
+     * This method bootstraps the JavaFX application.
+     *
+     * @param primaryStage JavaFX stage.
+     * @throws Exception
+     */
     @Override public void start(Stage primaryStage) throws Exception {
         this.stage = primaryStage;
+        this.stage.setTitle("Panthera");
         this.stage.setScene(new Scene(new TextField("Panthera")));
-        System.out.println("Panthera2");
+        this.stage.show();
+    }
+
+    /**
+     * Default main method which delegates to its super class.
+     *
+     * @param args Some commandline parameters.
+     */
+    public static void main(String args[]) {
+        launch(args);
     }
 }
