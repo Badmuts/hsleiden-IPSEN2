4fbf4d9477d0fb494ae28e0af9a7a24a19796649
Brandon van Wijk <brandonvanwijk@gmail.com>
2015-09-22 16:23:56 +0200

    factuurdao klasse aangemaakt met benodigde methodes

---
+++ src/DAO/FactuurDAO.java
@@ -0,0 +1,24 @@
+package DAO;
+
+/**
+ * Created by Brandon on 22-Sep-15.
+ */
+public class FactuurDAO {
+
+
+    public Factuur getFactuur(int id) {
+        //query om een specifieke factuur op te halen
+    }
+
+    public List<Factuur> getAllFacturen() {
+        //query om alle facturen op te halen
+    }
+
+    public void deleteFactuur(int id) {
+        //query om een factuur te verwijderen
+    }
+
+    public void saveFactuur() {
+        //query om een factuur op te slaan
+    }
+}
