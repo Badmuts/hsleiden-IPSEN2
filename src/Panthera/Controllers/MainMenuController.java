0057352d4bd1fc1e30981d6c08a67cb2b9829a4e
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-22 22:30:31 +0200

    Setup main menu view and controller. Still needs some work.

--- src/Panthera/Controllers/MainMenuController.java
+++ src/Panthera/Controllers/MainMenuController.java
@@ -1,12 +1,16 @@
 package Panthera.Controllers;
 
+import Panthera.Views.MainMenuView;
+
 /**
  * Created by Daan on 22-Sep-15.
  */
 public class MainMenuController {
 
-    public MainMenuController() {
+    private MainMenuView mainMenuView;
 
+    public MainMenuController() {
+        this.mainMenuView = new MainMenuView();
     }
 
 }
--- src/Panthera/Views/MainMenuView.java
+++ src/Panthera/Views/MainMenuView.java
@@ -1,17 +1,42 @@
 package Panthera.Views;
 
 import Panthera.Panthera;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 /**
  * Created by Daan on 22-Sep-15.
  */
-public class MainMenuView {
+public class MainMenuView extends BorderPane {
 
     private Stage stage = Panthera.getInstance().getStage();
+    private VBox menu;
 
     public MainMenuView() {
+        super();
+        this.createMenu();
+        setCenter(menu);
+        this.stage.setScene(new Scene(this));
+        this.stage.show();
+    }
 
+    /**
+     *
+     */
+    private void createMenu() {
+        this.menu = new VBox(10);
+        // Temp solution for button list.
+        ArrayList<Button> buttons = new ArrayList<>();
+        buttons.add(new Button("Facturen"));
+        buttons.add(new Button("Bestellijsten"));
+        buttons.add(new Button("Debiteuren"));
+        buttons.add(new Button("E-mail"));
+        for (Button button: buttons)
+            this.menu.getChildren().add(button);
     }
 
 }
