400ae474d9f830c35ea3cf8eb45bce8fb132d340
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-23 18:45:59 +0200

    Add Product DAO class

---
+++ src/Panthera/DAO/ProductDAO.java
@@ -0,0 +1,57 @@
+package Panthera.DAO;
+
+import Panthera.Models.Product;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ProductDAO extends DAO {
+
+    public ProductDAO() throws Exception {
+        super();
+    }
+
+    /**
+     * Get a Product model by id with values from the DB.
+     *
+     * @param id id of record in database.
+     * @return Product Product model.
+     * @throws Exception
+     */
+    public Product get(int id) throws Exception {
+        Product product = new Product();
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery("SELECT productnummer, naam, jaar, prijs, type FROM product WHERE id =" + id);
+        while (result.next()) {
+            product.setProductnummer(result.getInt("productnummer"));
+            product.setNaam(result.getString("naam"));
+            product.setJaar(result.getInt("jaar"));
+            product.setPrijs(result.getDouble("prijs"));
+            product.setType(result.getString("type"));
+        }
+        return product;
+    }
+
+    /**
+     * Returns a list with Product models with a limit of 25.
+     *
+     * @return ArrayList<Product> List with product models.
+     * @throws Exception
+     */
+    public ArrayList<Product> all() throws Exception {
+        ArrayList<Product> products = new ArrayList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery("SELECT productnummer, naam, jaar, prijs, type FROM product LIMIT 25");
+        while (result.next()) {
+            products.add(new Product(
+                result.getInt("productnummer"),
+                result.getString("naam"),
+                result.getInt("jaar"),
+                result.getDouble("prijs"),
+                result.getString("type")));
+        }
+        return products;
+    }
+
+}
