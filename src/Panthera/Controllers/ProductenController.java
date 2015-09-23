e9b8baf798ba98f56bcc71ec794f16e7e34446d9
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-23 18:46:58 +0200

    Add cmdGetProducten method to ProductenController

--- src/Panthera/Controllers/ProductenController.java
+++ src/Panthera/Controllers/ProductenController.java
@@ -1,11 +1,28 @@
 package Panthera.Controllers;
 
+import Panthera.DAO.ProductDAO;
+import Panthera.Models.Product;
 import Panthera.Views.ProductenListView;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class ProductenController extends Controller {
 
-    public ProductenController() {
+    private ProductDAO dao;
+
+    public ProductenController() throws Exception {
         this.view = new ProductenListView(this);
+        this.dao = new ProductDAO();
     }
 
+    public Collection<Product> cmdGetProducten() {
+        ArrayList<Product> products = new ArrayList<>();
+        try {
+            products.addAll(dao.all());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
 }
