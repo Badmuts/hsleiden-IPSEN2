f870d631a569a7c905e6e5e55ea48f78b9dd928f
Daan Rosbergen <d.rosbergen@gmail.com>
2015-09-24 22:28:25 +0200

    Add 'Node' to empty view to trigger refresh of stage

--- src/Panthera/Views/ProductenAddView.java
+++ src/Panthera/Views/ProductenAddView.java
@@ -3,6 +3,7 @@ package Panthera.Views;
 import Panthera.Panthera;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ProductenAddView extends BorderPane implements Viewable {
@@ -10,7 +11,8 @@ public class ProductenAddView extends BorderPane implements Viewable {
     private Stage stage = Panthera.getInstance().getStage();
 
     public ProductenAddView() {
-
+        Text title = new Text("Product toevoegen");
+        setTop(title);
     }
 
     @Override public void show() {
