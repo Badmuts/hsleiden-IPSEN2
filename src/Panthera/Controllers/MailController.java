2609ac6002b6013096ae319e6b3812037cf1fecf
Daan Rosbergen <d.rosbergen@gmail.com>
2015-10-01 15:31:08 +0200

    Remove test code and rename method

--- src/Panthera/Controllers/MailController.java
+++ src/Panthera/Controllers/MailController.java
@@ -1,6 +1,5 @@
 package Panthera.Controllers;
 
-import Panthera.Models.Email;
 import Panthera.Services.MailService;
 import Panthera.Services.Strategies.Mail.SendgridMailStrategy;
 import Panthera.Views.MailDankwoordView;
@@ -19,18 +18,10 @@ public class MailController extends Controller {
     public void cmdShowDankwoordView() {
         this.view = new MailDankwoordView(this);
         show();
-        Email email = new Email();
-        email.setTo("d.rosbergen@gmail.com");
-        email.setFrom("daan@daanrosbergen.nl");
-        email.setSubject("Java mail test");
-        email.setText("Hoi dit is Daan!");
-        email.addAttachment("daan.test", "daantestattachment.txt");
-        mailService.send(email);
-        System.out.println("Email send!");
     }
 
-    public Controller cmdSelectRecipients() {
+    public void cmdShowSelectRecipients() {
         this.view = new MailSelectRecipientsView(this);
-        return this;
+        show();
     }
 }
