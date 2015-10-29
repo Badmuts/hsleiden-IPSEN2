6185a3ebc162ab25bbbb254044c605bd634fb7bc
Daan Rosbergen <d.rosbergen@gmail.com>
2015-10-29 12:31:53 +0100

    Add comments to new class

--- src/Panthera/Views/Alerts/DatabaseErrorAlert.java
+++ src/Panthera/Views/Alerts/DatabaseErrorAlert.java
@@ -2,8 +2,21 @@ package Panthera.Views.Alerts;
 
 import javafx.scene.control.Alert;
 
+/**
+ * Class gebruikt om database foutmeldingen te tonen. Er wordt een leesbaar bericht getoont voor
+ * de gebruiker en een StackTrace van de Exception voor de programmeur.
+ *
+ * @author Daan Rosbergen
+ */
 public class DatabaseErrorAlert extends Alert {
 
+    /**
+     * Maakt een Alert en toont een leesbare foutmelding voor de gebruiker een een StackTrace voor
+     * de programmeur.
+     * 
+     * @param errorMessage  Leesbare foutmelding voor de gebruiker.
+     * @param e             Exception voor de programmeur.
+     */
     public DatabaseErrorAlert(String errorMessage, Exception e) {
         super(AlertType.ERROR);
         setHeaderText("Database fout!");
