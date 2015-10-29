4dc7e6def15ef761ec0c01109c22286e3787630d
Brandon van Wijk <brandonvanwijk@gmail.com>
2015-10-29 09:42:15 +0100

    Klasse toegevoegd die excel uitleest en het in een datatable zet

---
+++ src/Panthera/Models/ExportFromExcel.java
@@ -0,0 +1,37 @@
+package Panthera.Models;
+
+import com.aspose.cells.Workbook;
+import com.aspose.cells.Worksheet;
+import java.util.Arrays;
+import java.io.FileInputStream;
+
+/**
+ * Created by Brandon on 28-Oct-15.
+ */
+public class ExportFromExcel {
+
+
+
+        public static void main(String[] args) throws Exception
+        {
+            //Creating a file stream containing the Excel file to be opened
+            FileInputStream fstream = new FileInputStream("data/workbook.xls");
+
+            //Instantiating a Workbook object
+            Workbook workbook = new Workbook(fstream);
+
+            //Accessing the first worksheet in the Excel file
+            Worksheet worksheet = workbook.getWorksheets().get(0);
+
+            //Exporting the contents of 7 rows and 2 columns starting from 1st cell to Array.
+            Object dataTable [][] = worksheet.getCells().exportArray(4,0,7,8);
+
+            for (int i = 0 ; i < dataTable.length ; i++)
+            {
+                System.out.println("["+ i +"]: "+ Arrays.toString(dataTable[i]));
+            }
+            //Closing the file stream to free all resources
+            fstream.close();
+        }
+    }
+
