09ca728b63f878811566cddec6b2c09f24d0c21e
Daan Rosbergen <d.rosbergen@gmail.com>
2015-11-10 00:05:55 +0100

    Add comments to decorators

--- src/Panthera/Services/Decorators/DebiteurParser.java
+++ src/Panthera/Services/Decorators/DebiteurParser.java
@@ -3,6 +3,15 @@ package Panthera.Services.Decorators;
 import Panthera.Models.Debiteur;
 import Panthera.Models.Model;
 
+/**
+ * Parser for debiteur models, used for replace codes in for example emails or templates. This
+ * parser uses the decorator pattern.
+ *
+ * Replace codes:
+ * - `{LID}` Example: Daan Rosbergen or Brandon van Wijk.
+ *
+ * @author Daan Rosbergen
+ */
 public class DebiteurParser extends Parser {
 
     private Model parser;
@@ -12,11 +21,25 @@ public class DebiteurParser extends Parser {
         this.parser = parser;
     }
 
+    /**
+     * Parse replace codes for debiteur.
+     *  - {LID}: Example: Daan Rosbergen or Brandon van Wijk.
+     *
+     * @param text
+     * @param data
+     * @return
+     */
     @Override public String parse(String text, Model data) {
         this.debiteur = (Debiteur)data;
-        return parser.parse(text, parser).replace("{LID}", debiteur.getAanhef() + " " + debiteur.getVoornaam() + " " + parseTussenvoegsel() + debiteur.getNaam());
+        return parser.parse(text, parser).replace("{LID}", debiteur.getAanhef() + " " +
+            debiteur.getVoornaam() + " " + parseTussenvoegsel() + debiteur.getNaam());
     }
 
+    /**
+     * Checks if tussenvoegsel is null and parses it accordingly.
+     *
+     * @return
+     */
     private String parseTussenvoegsel() {
         String tussenvoegsel = "";
         if (debiteur.getTussenvoegsel() != null) {
--- src/Panthera/Services/Decorators/Parser.java
+++ src/Panthera/Services/Decorators/Parser.java
@@ -2,6 +2,10 @@ package Panthera.Services.Decorators;
 
 import Panthera.Models.Model;
 
+/**
+ * Decorator component used for parsing replace codes in strings.
+ *
+ */
 public abstract class Parser extends Model {
 
     public abstract String parse(String text, Model data);
