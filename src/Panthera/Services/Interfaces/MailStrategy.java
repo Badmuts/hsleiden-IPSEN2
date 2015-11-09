b11d52aa57b3c0a371f7db438fbfb610394cc8a3
Daan Rosbergen <d.rosbergen@gmail.com>
2015-11-10 00:12:53 +0100

    Add comments to mail strategies

--- src/Panthera/Services/Interfaces/MailStrategy.java
+++ src/Panthera/Services/Interfaces/MailStrategy.java
@@ -2,8 +2,18 @@ package Panthera.Services.Interfaces;
 
 import Panthera.Models.Email;
 
+/**
+ * Strategy pattern used to change from MailStrategy during runtime.
+ *
+ * @author Daan Rosbergen
+ */
 public interface MailStrategy {
 
+    /**
+     * Method used to send a Email object.
+     *
+     * @param email
+     */
     public void send(Email email);
 
 }
