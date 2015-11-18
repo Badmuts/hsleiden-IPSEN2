f246767dcfdb7aaec3bebdd5c5a6a6110d630c20
Brandon van Wijk <brandonvanwijk@gmail.com>
2015-11-18 13:10:41 +0100

    added javadoc to FactuurDAO class

--- src/Panthera/DAO/FactuurDAO.java
+++ src/Panthera/DAO/FactuurDAO.java
@@ -10,14 +10,33 @@ import java.util.ArrayList;
 
 
 /**
+ * @author Brandon
  * Created by Brandon on 22-Sep-15.
+ * Deze klasse wordt gebruikt om gegevens op te halen uit de database
+ * Maar ook om gegevens op te slaan in de database
  */
 public class FactuurDAO extends DAO {
 
+    /**
+     * @author Brandon van Wijk
+     * Dit is de standaard constructor om een instantie te kunnen maken van de DAO
+     * @throws IllegalAccessException
+     * @throws SQLException
+     * @throws InstantiationException
+     */
     public FactuurDAO() throws IllegalAccessException, SQLException, InstantiationException {
         super();
     }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode haalt 1 specifieke factuur op uit de database aan de hand van het id
+     * Vervolgens wordt er een nieuwe factuur object aangemaakt
+     * En gevuld met de data vanuit de database
+     * @param id
+     * @return
+     * @throws Exception
+     */
     public Factuur getFactuur(int id) throws Exception {
         //query om een specifieke factuur op te halen
         Factuur factuur = new Factuur();
@@ -34,16 +53,30 @@ public class FactuurDAO extends DAO {
         return factuur;
     }
 
-        public void updateStatus(Factuur factuur , String status) throws Exception {
+    /**
+     * @author Brandon van Wijk
+     * Deze methode krijgt een factuur object mee en een string
+     * De factuur is om te specifieceren om welke factuur het gaat
+     * De string is de nieuwe status die aan de factuur gegeven wordt
+     * Vervolgens wordt de factuur geupdatet in de database
+     * @param factuur
+     * @param status
+     * @throws Exception
+     */
+    public void updateStatus(Factuur factuur , String status) throws Exception {
             Statement stmt = conn.createStatement();
             stmt.executeUpdate("UPDATE factuur " +
                     "SET status='"+ status +"' WHERE id=" + factuur.getId());
-        }
+    }
 
     /**
-     * Returns a list with Factuur models.
-     *
-     * @return ArrayList<Factuur> List with product models.
+     * @author Brandon van Wijk
+     * @author Daan Rosbergen
+     * Deze methode haalt alle factuur info op uit de database
+     * Zowel de info van de order en de info van de producten op de order
+     * Vervolgens wordt de lijst factuurregels die zich in factuur bevindt
+     * Gevuld met de benodigde data uit de database
+     * @return ArrayList<Factuur> List met factuurmodellen
      * @throws Exception
      */
     public ArrayList<Factuur> getAllFacturen() throws Exception {
@@ -89,13 +122,29 @@ public class FactuurDAO extends DAO {
         return facturen;
     }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode verwijdert een factuur uit de database
+     * Er komt een factuur object binnen als parameter om
+     * Duidelijk te maken om welke factuur het gaat aan de hand van zijn id
+     * @param factuur
+     * @throws SQLException
+     */
     public void deleteFactuur(Factuur factuur) throws SQLException {
         //query om een factuur te verwijderen
        Statement stmt = conn.createStatement();
         stmt.executeUpdate("DELETE FROM factuur " +
                 "WHERE id=" + factuur.getId());
-        }
+    }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode slaat een factuur op in de database
+     * Er komt een factuur object binnen als parameter
+     * Om zo de data uit het object op te kunnen halen
+     * @param factuur
+     * @throws Exception
+     */
     public void save(Factuur factuur) throws Exception {
         Statement stmt = conn.createStatement();
         stmt.executeUpdate("" +
@@ -111,6 +160,15 @@ public class FactuurDAO extends DAO {
         saveFactuurregels(factuur);
     }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode slaat de factuurregels van een object op
+     * Zoals het factuur id, het aantal aangegeven wijnen op de factuur en het product id
+     * De methode krijgt een factuur object binnen om de benodigde data te kunnen vragen
+     * Aan het factuur object
+     * @param factuur
+     * @throws Exception
+     */
     private void saveFactuurregels(Factuur factuur) throws Exception {
         int factuurId = getLastFactuurId();
         for (Factuurregel factuurregel: factuur.getFactuurregels()) {
@@ -124,6 +182,15 @@ public class FactuurDAO extends DAO {
         }
     }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode haalt het laatste id op
+     * Dat is toegevoegd in de factuur tabel
+     * Vervolgens wordt er bij dat id 1 opgeteld
+     * Dit wordt gedaan zodat we geen conflicts krijgen met id's
+     * @return id
+     * @throws Exception
+     */
     public int getLastFactuurId() throws Exception {
         int id = 0;
         Statement stmt = conn.createStatement();
@@ -134,6 +201,14 @@ public class FactuurDAO extends DAO {
         return id++;
     }
 
+    /**
+     * @author Brandon van Wijk
+     * Deze methode haalt het laatste factuurnummer op uit de database
+     * En telt er 1 bij op dit doen we omdat we zo een default factuurnummer
+     * kunnen laten zien bij het aanmaken van een factuur in de applicatie
+     * @return
+     * @throws Exception
+     */
     public int getLastFactuurNummer() throws Exception {
         int factuurnummer = 0;
 
